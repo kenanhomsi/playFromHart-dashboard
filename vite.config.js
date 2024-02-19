ba4550d1ef3922bb17c3956839564fbdb6ddfe32
kenanhomsi <kinanhomsi0959@gmail.com>
2024-02-19 16:21:13 +0300

    second

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/playFromHart-dashboard/",
   plugins: [react()],
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  }
 })
